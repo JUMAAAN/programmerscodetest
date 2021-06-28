809fc0e53c762e3259b727a0178a0bb1ef01aeb0
JUMAAAN <smsky53@skku.edu>
2021-06-28 14:44:41 +0900

    Create 디스크_컨트롤러_다른풀이.py
    다른 사람 풀이 해석

---
+++ 힙트리/디스크_컨트롤러_다른풀이.py
@@ -0,0 +1,25 @@
+import heapq
+def solution(jobs):
+    pool = []
+    elapsted_t = 0
+    current_t = 0
+    initial_t = 0
+    n = len(jobs)
+    heapq.heapify(pool)
+    jobs = sorted(jobs, key=lambda x: x[0])
+    while jobs or pool :
+        while jobs :
+            if current_t >= jobs[0][0]:
+                st, et = jobs.pop(0)
+                initial_t += st
+                heapq.heappush(pool, et)
+            else:
+                break
+        if pool :
+            et = heapq.heappop(pool)
+            current_t += et
+            elapsted_t += current_t
+        else:
+            current_t = jobs[0][0]
+    answer = (elapsted_t-initial_t) // n
+    return answer
