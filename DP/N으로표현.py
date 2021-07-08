a6bbe0f850753f3bf1195acc0c6058aad16d692e
JUMAAAN <smsky53@skku.edu>
2021-07-08 16:19:57 +0900

    Create N으로표현.py
    집가서 풀기

---
+++ DP/N으로표현.py
@@ -0,0 +1,14 @@
+def solution(N,number):
+      answer=1
+      dp=[]
+      for i in range(8):#최대 8
+            dp.append([])
+      dp[0].append(N)#F1=N, F1=dp[0] 1차이
+      dp[1]=[N+N,N-N,N*N,N/N,N*11]# F2 F2=dp[1]
+      chek=1
+      
+
+def docalculate(dp,i):
+      
+                        
+solution(5,12)
