0bee7b45a925cd1f000635cd38cffe1d1d2f4686
JUMAAAN <smsky53@skku.edu>
2021-07-12 19:14:50 +0900

    Create 등굣길.py
    답이랑 머가 다른지 확인

---
+++ DP/등굣길.py
@@ -0,0 +1,16 @@
+def solution(m, n, puddles):
+    answer=0
+    matrix=[[0]*(m+1) for x in range(n+1)]
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if i==1 and j==1:
+                matrix[1][1]=1 #시작점
+            elif [i,j] in puddles:
+                matrix[i][j]=0#웅덩이
+            else:
+                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]
+    print(matrix)
+    answer=matrix[n][m]%1000000007
+    return answer
+
+print(solution(5,4,[[2,1],[2,2],[2,3],[4,4],[4,3],[4,2]]))
