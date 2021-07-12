65422a510d7340b8c845511c10ab58aede01dd87
JUMAAAN <smsky53@skku.edu>
2021-07-12 22:54:25 +0900

    Update 도둑질.py
    해결. 정답

--- DP/도둑질.py
+++ DP/도둑질.py
@@ -5,11 +5,12 @@ def solution(money):
     for i in range(1,3):
         dp1.append(money[-i])
         dp2.append(money[-i-1])
+    dp1[1]=max(dp1)
+    dp2[1]=max(dp2)
     for i in range(2,len(money)-1):
         dp1.append(max(dp1[i-2]+money[-i-1],dp1[i-1]))
         dp2.append(max(dp2[i-2]+money[-i-2],dp2[i-1]))
-        print(dp1,dp2)
     answer=dp1+dp2
     answer=max(answer)
     return answer
-print(solution([1,2,3,1]))
+print(solution( [4, 6, 4, 3, 7]))
