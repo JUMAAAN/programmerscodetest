9f92e568560687a58b30e6a241a5667311fe8dfd
JUMAAAN <smsky53@skku.edu>
2021-07-15 13:28:28 +0900

    Update 여행경로.py
    index나 depth로 푸는게 나을듯?
    너무 deepcopy가 많음. 많은 주소사용..

--- dfs_bfs/여행경로.py
+++ dfs_bfs/여행경로.py
@@ -1,25 +1,32 @@
+import copy as cp
+
 def solution(tickets):
       answer=[]
       firstnode=[]
+      temp=[]
       for i in tickets:
             if i[0]=='ICN':
                   firstnode.append(i) #인천출발
-
       for i in firstnode:
-            answer+=dfs(i,tickets)
+            stack=['ICN']
+            dfs(i,tickets,stack,temp)
+      answer=min(temp)
       return answer
 
 
-def dfs(listnode,tickets):#listnode=[출발,도착]
-      visted=tickets
-      visted.remove(listnode)
-      temp=[]
-      if visted==[]:#올바른 경로
-            return listnode[1]
-      arrive=listnode[1]
-      for i in visted:
-            if i[0]==arrive:
-                  temp.append(listnode[1]+dfs(i,visted))
-      return temp
-
+def dfs(listnode,tickets,stack,temp):#listnode=[출발,도착]
+      clone=cp.deepcopy(tickets)
+      clone.remove(listnode)
+      arri=listnode[1]
+      stack.append(arri)
+      for i in clone:
+            if i[0]==arri:
+                  dfs(i,clone,stack,temp)
+      if clone==[]:
+            tmp=cp.deepcopy(stack)
+            temp.append(tmp)
+            stack.pop()
+      else:
+            stack.pop()
+            
 solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]])
